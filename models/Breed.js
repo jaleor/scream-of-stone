f41967ac7339881fcdaaa064ff929db945531d99
Javier Orsaria <javier.orsaria@gmail.com>
2019-05-19 18:22:02 -0300

    Breeds

---
+++ models/Breed.js
@@ -0,0 +1,19 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Breed Model
+ * ==========
+ */
+var Breed = new keystone.List('Breed');
+
+Breed.add({
+	name: { type: String, required: true, index: true, initial: true },
+	specie:{ label: 'Especie', type: Types.Relationship, ref: 'Specie', createInline: true, required: true, initial: true }
+});
+
+/**
+ * Registration
+ */
+Breed.defaultColumns = 'name, specie';
+Breed.register();
--- models/Pet.js
+++ models/Pet.js
@@ -28,7 +28,7 @@ Pet.add({
 	name: { label: 'Nombre', type: String, required: true, index: true, initial: true },
 	owner: { label: 'Dueño', type: Types.Relationship, ref: 'Owner', createInline: true, required: true, initial: true },
 	specie:{ label: 'Especie', type: Types.Relationship, ref: 'Specie', createInline: true, required: true, initial: true },
-	breed: { label: 'Raza', type: Types.Select, options: 'mestizo, caniche'},
+	breed: { label: 'Raza', type: Types.Relationship, ref: 'Breed', filters: { specie: ':specie' }, createInline: true, required: false, initial: false },
 	gender: { label: 'Género', type: Types.Select, options: 'hembra, macho', default: 'hembra' },
 	/*}, 'Castrado/a', {*/isCastrated: { label: 'Castrado/a', type: Types.Boolean },
 	castratedDate: { label: 'Fecha de castración', dependsOn: { isCastrated: true }, type: Types.Date, format: 'DD/MM/YYYY', inputFormat: 'DD/MM/YYYY' },
